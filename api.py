dd30d62b7de8a41c8be5e3bad9bd99e84bb987fb
tochnonemasha <164243846+tochnonemasha@users.noreply.github.com>
2026-07-10 23:45:59 +0300

    api.py

--- api.py
+++ api.py
@@ -402,3 +402,6 @@ def status(db=Depends(get_db)):
             "interactions": n_interactions,
         }
     }
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
